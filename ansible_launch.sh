19a9c29ac9d95bd71526960faf5ac02aa5617bb0
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-07-19 20:16:00 +0200

    save

--- ansible_launch.sh
+++ ansible_launch.sh
@@ -1,2 +1,2 @@
-ansible-playbook instances_spawn.yml --private-key ~/.ssh/amichaud_xebia.pem
+ansible-playbook instances_spawn.yml --private-key ~/.ssh/amichaud_xebia.pem && \
 ansible-playbook instances_config.yml --private-key ~/.ssh/amichaud_xebia.pem -i inventory/ec2.py
